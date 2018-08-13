6c7a201ffc63b292c8b74548ec128589ab32138d
minhlee <minhlee.fre.mta@gmail.com>
2018-08-13 10:52:14 +0700

    add method borrowBook()

--- src/main/java/BookBorrower.java
+++ src/main/java/BookBorrower.java
@@ -2,5 +2,15 @@ public class BookBorrower {
     private BookSingleton borrowedBook;
     private boolean haveBook = false;
 
+    public void borrowBook() {
+        borrowedBook = BookSingleton.borrowBook();
+
+        if (borrowedBook == null) {
+            haveBook = false;
+        } else {
+            haveBook = true;
+        }
+    }
+
 
 }
